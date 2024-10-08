938c69fd0d6b0b1be5f904878a2af10761849392
Özkan KÜTÜK <ozkan.kutuk@gmail.com>
2024-10-08 12:15:51 +0300

    storage tamamlandı

--- database/seeders/LabelSeeder.php
+++ database/seeders/LabelSeeder.php
@@ -20,26 +20,11 @@ class LabelSeeder extends Seeder
         DB::table('labels')->truncate();
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
-        $disks = config('filesystems.disks');
-
-        Tenant::chunk(100, function ($tenants) use (&$disks) {
-            foreach ($tenants as $tenant) {
-                $disks['tenant_'.$tenant->id] = [
-                    'driver' => 'local',
-                    'root' => storage_path('app/public/tenant_'.$tenant->id),
-                    'url' => env('APP_URL').'/storage/tenant_'.$tenant->id,
-                    'visibility' => 'public',
-                ];
-            }
-        });
-
-        config(['filesystems.disks' => $disks]);
-
-        Label::factory(15)->create()->each(function (Label $label) use ($tenant) {
+        Label::factory(15)->create()->each(function (Label $label) {
             $label->addMediaFromUrl('https://picsum.photos/400/400')
                 ->usingFileName(Str::slug($label->name).'.jpg')
                 ->usingName($label->name)
-                ->toMediaCollection('labels', 'tenant_'.$tenant->id);
+                ->toMediaCollection('labels', 'tenant_'.tenant('id'));
         });
     }
 }
