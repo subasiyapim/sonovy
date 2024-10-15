24e1e17707233bdbd42780a23e3bf4191c518a9b
Özkan KÜTÜK <ozkan.kutuk@gmail.com>
2024-10-15 15:04:07 +0300

    reverb kurulumu tamamlandı

--- resources/js/Components/Form/FormElement.vue
+++ resources/js/Components/Form/FormElement.vue
@@ -1,92 +1,97 @@
 <template>
-    <div class="flex items-start" :class="direction == 'horizontal' ? 'flex-row' : 'flex-col mb-2'">
-        <div class="flex items-center gap-1" :style="`width:${labelWidth}`">
-                <label class="label-sm c-text-solid-950">{{label}}</label>
-                <span v-if="required" class="c-blue-500 label-sm">*</span>
-                 <tippy v-if="hasSlot('tooltip')">
-                    <InfoFilledIcon  color="var(--soft-300)" />
+  <div class="flex items-start" :class="direction == 'horizontal' ? 'flex-row' : 'flex-col mb-2'">
+    <div class="flex items-center gap-1" :style="`width:${labelWidth}`">
+      <label class="label-sm c-text-solid-950">{{ label }}</label>
+      <span v-if="required" class="c-blue-500 label-sm">*</span>
+      <tippy v-if="hasSlot('tooltip')">
+        <InfoFilledIcon color="var(--soft-300)"/>
 
-                    <template #content>
-                        <slot name="tooltip" />
-                    </template>
-                </tippy>
-
-        </div>
-        <div class="w-full" :class="error ? 'hasError' : ''">
-
-             <AppTextInput v-if="type=='text' || type=='web' || type=='phone'|| type=='password'" :type="type" v-model="element" :placeholder="placeholder"></AppTextInput>
-             <AppTextareaInput v-else-if="type=='textarea'" v-model="element" :placeholder="placeholder" :config="config"></AppTextareaInput>
-                <AppUploadInput v-else-if="type=='upload'" :label="config.label" :note="config.note" v-model="element" :placeholder="placeholder"></AppUploadInput>
-             <AppSelectInput v-else-if="type=='select'" :config="config" :type="type" v-model="element" :placeholder="placeholder">
-               <template v-if="hasSlot('first_child')" #first_child>
-                     <slot  name="first_child" />
-               </template>
-               <template v-if="hasSlot('empty')" #empty>
-                     <slot name="empty" />
-               </template>
-             </AppSelectInput>
-
-             <AppMultiSelectInput v-else-if="type=='multiselect'" :config="config" :type="type" v-model="element" :placeholder="placeholder">
-               <template v-if="hasSlot('first_child')" #first_child>
-                     <slot  name="first_child" />
-               </template>
-               <template v-if="hasSlot('empty')" #empty>
-                     <slot name="empty" />
-               </template>
-             </AppMultiSelectInput>
-
-
-            <slot name="description" />
-            <span v-if="error" class="c-error-500 paragraph-xs flex items-center gap-1 mt-2">
-                <InfoFilledIcon color="var(--error-500)" />
-                {{error}}
-            </span>
-       </div>
+        <template #content>
+          <slot name="tooltip"/>
+        </template>
+      </tippy>
 
     </div>
+    <div class="w-full" :class="error ? 'hasError' : ''">
+
+      <AppTextInput v-if="type=='text' || type=='web' || type=='phone'|| type=='password'" :type="type"
+                    v-model="element" :placeholder="placeholder"></AppTextInput>
+      <AppTextareaInput v-else-if="type=='textarea'" v-model="element" :placeholder="placeholder"
+                        :config="config"></AppTextareaInput>
+      <AppUploadInput v-else-if="type=='upload'" :label="config.label" :note="config.note" v-model="element"
+                      :placeholder="placeholder"></AppUploadInput>
+      <AppSelectInput v-else-if="type=='select'" :config="config" :type="type" v-model="element"
+                      :placeholder="placeholder">
+        <template v-if="hasSlot('first_child')" #first_child>
+          <slot name="first_child"/>
+        </template>
+        <template v-if="hasSlot('empty')" #empty>
+          <slot name="empty"/>
+        </template>
+      </AppSelectInput>
+
+      <AppMultiSelectInput v-else-if="type=='multiselect'" :config="config" :type="type" v-model="element"
+                           :placeholder="placeholder">
+        <template v-if="hasSlot('first_child')" #first_child>
+          <slot name="first_child"/>
+        </template>
+        <template v-if="hasSlot('empty')" #empty>
+          <slot name="empty"/>
+        </template>
+      </AppMultiSelectInput>
+
+
+      <slot name="description"/>
+      <span v-if="error" class="c-error-500 paragraph-xs flex items-center gap-1 mt-2">
+                <InfoFilledIcon color="var(--error-500)"/>
+                {{ error }}
+            </span>
+    </div>
+
+  </div>
 
 </template>
 
 <script setup>
 import {computed} from 'vue';
 import {InfoFilledIcon} from '@/Components/Icons'
-import AppTextInput from './ApptextInput.vue';
+import AppTextInput from './AppTextInput.vue';
 import AppTextareaInput from './AppTextareaInput.vue';
 import AppSelectInput from './AppSelectInput.vue';
 import AppMultiSelectInput from './AppMultiSelectInput.vue';
 import AppUploadInput from './AppUploadInput.vue';
 
-import { useSlots } from 'vue';
+import {useSlots} from 'vue';
 
 
 const props = defineProps({
-    direction:{
-        default:'horizontal', //vertical
-    },
-    required:{
-        default:false,
-    },
-    error:{
-        default:null,
-    },
-    label:{},
-    labelWidth:{
-        default:'120px'
-    },
-    placeholder:{},
-    modelValue:{},
-    type:{type:String,default:"text"},
-    config:{type:Object}
+  direction: {
+    default: 'horizontal', //vertical
+  },
+  required: {
+    default: false,
+  },
+  error: {
+    default: null,
+  },
+  label: {},
+  labelWidth: {
+    default: '120px'
+  },
+  placeholder: {},
+  modelValue: {},
+  type: {type: String, default: "text"},
+  config: {type: Object}
 })
 const emits = defineEmits(['update:modelValue']);
 
 const element = computed({
-    get:() => props.modelValue,
-    set:(value) => emits('update:modelValue',value)
+  get: () => props.modelValue,
+  set: (value) => emits('update:modelValue', value)
 })
 const slots = useSlots()
 const hasSlot = (name) => {
-    return !!slots[name];
+  return !!slots[name];
 }
 </script>
 
--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -2,38 +2,44 @@
 import AuthLayout from '@/Layouts/AuthLayout.vue'
 import {FormElement} from '@/Components/Form'
 import {PrimaryButton} from '@/Components/Buttons'
-import { Head, Link, useForm } from '@inertiajs/vue3';
-import { AddIcon} from '@/Components/Icons';
+import {Head, Link, useForm} from '@inertiajs/vue3';
+import {AddIcon} from '@/Components/Icons';
+
 const form = useForm({
-    email: '',
-    password: '',
-    remember: false,
+  email: '',
+  password: '',
+  remember: false,
 });
+
+const submit = () => {
+  form.post(route('register'));
+}
 </script>
 
 <template>
-    <AuthLayout>
-         <h1  class="label-xl c-strong-950 !text-center"> Ücretsiz Hesap Oluştur </h1>
-         <p class="paragraph-sm c-sub-600 !text-center mb-6"> Hızlıca yayın oluşturmak için ücretsiz hesabınızı oluşturun </p>
-        <form @submit.prevent="submit" class="flex flex-col gap-3">
-           <div class="flex gap-2 items-center">
-                <FormElement class="flex-1" direction="vertical" placeholder="Adı" label="Ad"></FormElement>
-                <FormElement class="flex-1" direction="vertical" placeholder="Soyadı" label="Soyad"></FormElement>
-           </div>
-            <FormElement direction="vertical" label="E-mail" placeholder="hello@muzikdagitim.com" type="text"></FormElement>
-            <FormElement direction="vertical" label="Telefon" placeholder="(555) 000-0000" type="phone"></FormElement>
-            <FormElement direction="vertical" label="Şifre" placeholder="• • • • • • • • • • " type="password"></FormElement>
-            <PrimaryButton>
-                <template #icon>
-                    <AddIcon />
-                </template>
-                Giriş Yap
-            </PrimaryButton>
-            <div class="flex items-center gap-1 justify-center my-3">
-                    <p class="label-xs c-sub-600">Zaten hesabım var?</p>
-                    <a href="/login" class="label-xs c-dark-green-600">Giriş yap</a>
-            </div>
-        </form>
-    </AuthLayout>
+  <AuthLayout>
+    <h1 class="label-xl c-strong-950 !text-center"> Ücretsiz Hesap Oluştur </h1>
+    <p class="paragraph-sm c-sub-600 !text-center mb-6"> Hızlıca yayın oluşturmak için ücretsiz hesabınızı
+      oluşturun </p>
+    <form @submit.prevent="submit" class="flex flex-col gap-3">
+      <div class="flex gap-2 items-center">
+        <FormElement class="flex-1" direction="vertical" placeholder="Adı" label="Ad"></FormElement>
+        <FormElement class="flex-1" direction="vertical" placeholder="Soyadı" label="Soyad"></FormElement>
+      </div>
+      <FormElement direction="vertical" label="E-mail" placeholder="hello@muzikdagitim.com" type="text"></FormElement>
+      <FormElement direction="vertical" label="Telefon" placeholder="(555) 000-0000" type="phone"></FormElement>
+      <FormElement direction="vertical" label="Şifre" placeholder="• • • • • • • • • • " type="password"></FormElement>
+      <PrimaryButton>
+        <template #icon>
+          <AddIcon/>
+        </template>
+        Giriş Yap
+      </PrimaryButton>
+      <div class="flex items-center gap-1 justify-center my-3">
+        <p class="label-xs c-sub-600">Zaten hesabım var?</p>
+        <a href="/login" class="label-xs c-dark-green-600">Giriş yap</a>
+      </div>
+    </form>
+  </AuthLayout>
 
 </template>
