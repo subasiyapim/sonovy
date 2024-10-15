9031b1dd79b0c84d79c8ffb29030a6e1d2c97b50
Özkan KÜTÜK <ozkan.kutuk@gmail.com>
2024-10-15 17:26:21 +0300

    DB oluşturma adımları tamamlandı.

--- database/seeders/System/TenantSeeder.php
+++ database/seeders/System/TenantSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders\System;
 
 use App\Models\System\Tenant;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class TenantSeeder extends Seeder
 {
@@ -22,13 +23,25 @@ class TenantSeeder extends Seeder
             $tenant->delete();
         });
 
+
         foreach (['app', 'demo'] as $domain) {
+            
             $uniq_id = uniqid();
+            $db_name = 'tenant_'.$domain.'_'.$uniq_id;
+            $db_user = 'tenant_'.$domain.'_'.$uniq_id;
+            $db_password = uniqid();
+
+            DB::statement("CREATE DATABASE $db_name");
+            DB::statement("CREATE USER '$db_user'@'localhost' IDENTIFIED BY '$db_password'");
+            DB::statement("GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%'");
+            DB::statement("FLUSH PRIVILEGES");
 
             $tenant = Tenant::create([
                 'id' => (string) $uniq_id,
                 'name' => $domain,
-                'tenancy_db_name' => 'tenant_'.$domain.'_'.$uniq_id,
+                'tenancy_db_name' => $db_name,
+                'tenancy_db_username' => $db_user,
+                'tenancy_db_password' => $db_password,
             ]);
 
             $tenant->domains()->create(['domain' => $domain]);
