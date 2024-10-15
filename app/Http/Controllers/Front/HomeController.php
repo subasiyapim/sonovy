e380fb691f899217af7bd49e6c7df16244726092
Özkan KÜTÜK <ozkan.kutuk@gmail.com>
2024-10-15 22:01:21 +0300

    updated

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -16,6 +16,7 @@ class HomeController extends Controller
 {
     public function index()
     {
+        dd(tenant('id'));
         $site_banner = Setting::where('key', 'site_banner')->first()?->value;
         $partners = Partner::all();
         $site_features = SiteFeature::all();
