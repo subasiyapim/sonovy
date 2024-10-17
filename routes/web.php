7afd83c42782fb41cc13a30c3a12510df62cf728
Özkan KÜTÜK <ozkan.kutuk@gmail.com>
2024-10-17 19:07:05 +0300

    updated

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Front\HomeController;
 use App\Http\Controllers\Front\SupportController;
@@ -18,9 +19,10 @@ Route::group(
 
 
     Route::get('new-tenant', function (\Illuminate\Http\Request $request) {
-        $name = $request->get('name');
+        $domain = $request->get('name');
         $uniq_id = uniqid();
-        $db_user = 'tenant_'.$name.'_'.$uniq_id;
+        $db_name = 'tenant_'.$domain.'_'.Carbon::now()->format('d_m_Y_H_i_s');
+        $db_user = 'tenant_'.$domain.'_'.$uniq_id;
         $db_password = uniqid();
         $domain = \Stancl\Tenancy\Database\Models\Domain::where('domain', $request->name)->exists();
 
@@ -33,24 +35,24 @@ Route::group(
         DB::statement("CREATE USER '$db_user'@'localhost' IDENTIFIED BY '$db_password'");
 
         // Kullanıcıya localhost için tüm yetkileri ver
-        DB::statement("GRANT ALL PRIVILEGES ON 'tenant_'.$name.'_'.$uniq_id.* TO '$db_user'@'localhost'");
+        DB::statement("GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'localhost'");
 
         // Yetkileri uygula
         DB::statement("FLUSH PRIVILEGES");
-        
+
         $tenant = \App\Models\System\Tenant::create(
             [
                 'id' => (string) $uniq_id,
-                'tenancy_db_name' => 'tenant_'.$name.'_'.$uniq_id,
-                'name' => 'tenant_'.$name.'_'.$uniq_id,
+                'tenancy_db_name' => $db_name,
+                'name' => 'tenant_'.$domain,
                 'tenancy_db_username' => $db_user,
                 'tenancy_db_password' => $db_password,
             ]
         );
 
-        $tenant->domains()->create(['domain' => $name]);
+        $tenant->domains()->create(['domain' => $domain]);
 
-        return $name.'.'.env('BASE_URL').' created';
+        return $domain.'.'.env('BASE_URL').' created';
     });
 
 });
