eaac5135d8e47d87707e1b317497b87a7000e5d4
Özkan KÜTÜK <ozkan.kutuk@gmail.com>
2024-10-17 18:50:42 +0300

    updated

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,26 @@ Route::group(
     ], function () {
 
 
+    Route::get('new-tenant', function (\Illuminate\Http\Request $request) {
+        $name = $request->get('name');
+        $uniq_id = uniqid();
+        $domain = \Stancl\Tenancy\Database\Models\Domain::where('domain', $request->name)->exists();
+
+        if ($domain) {
+            return 'Domain already exists';
+        }
+
+        $tenant = \App\Models\System\Tenant::create(
+            [
+                'id' => (string) $uniq_id,
+                'tenancy_db_name' => 'tenant_'.$name.'_'.$uniq_id,
+                'name' => 'tenant_'.$name.'_'.$uniq_id
+            ]
+        );
+
+        $tenant->domains()->create(['domain' => $name]);
+
+        return $name.'.'.env('BASE_URL').' created';
+    });
+
 });
