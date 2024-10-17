292a9a325dab3a89349252895d531ae43bcc91f0
Özkan KÜTÜK <ozkan.kutuk@gmail.com>
2024-10-17 15:29:35 +0300

    updated

--- database/seeders/System/TenantSeeder.php
+++ database/seeders/System/TenantSeeder.php
@@ -6,6 +6,7 @@ use App\Models\System\Tenant;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Carbon\Carbon;
 
 class TenantSeeder extends Seeder
 {
