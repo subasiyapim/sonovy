0e0dd17417c620c207271875bf3f84616825ea66
Özkan KÜTÜK <ozkan.kutuk@gmail.com>
2024-10-17 17:22:56 +0300

    updated

--- app/Providers/TenantDiskServiceProvider.php
+++ app/Providers/TenantDiskServiceProvider.php
@@ -20,19 +20,21 @@ class TenantDiskServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $disks = config('filesystems.disks');
+        if (\Schema::hasTable('tenants')) {
+            $disks = config('filesystems.disks');
 
-        Tenant::chunk(100, function ($tenants) use (&$disks) {
-            foreach ($tenants as $tenant) {
-                $disks['tenant_'.$tenant->id] = [
-                    'driver' => 'local',
-                    'root' => storage_path('app/public/tenant_'.$tenant->id),
-                    'url' => env('APP_URL').'/storage/tenant_'.$tenant->id,
-                    'visibility' => 'public',
-                ];
-            }
-        });
+            Tenant::chunk(100, function ($tenants) use (&$disks) {
+                foreach ($tenants as $tenant) {
+                    $disks['tenant_'.$tenant->id] = [
+                        'driver' => 'local',
+                        'root' => storage_path('app/public/tenant_'.$tenant->id),
+                        'url' => env('APP_URL').'/storage/tenant_'.$tenant->id,
+                        'visibility' => 'public',
+                    ];
+                }
+            });
 
-        config(['filesystems.disks' => $disks]);
+            config(['filesystems.disks' => $disks]);
+        }
     }
 }
