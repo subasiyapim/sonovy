bbe3cfc2519fdb0461dae6e9298bbdace2b695cf
Özkan KÜTÜK <ozkan.kutuk@gmail.com>
2024-10-17 14:09:26 +0300

    updated

--- database/seeders/System/TenantSeeder.php
+++ database/seeders/System/TenantSeeder.php
@@ -18,13 +18,10 @@ class TenantSeeder extends Seeder
     public function run()
     {
 
-//        Tenant::get()->each(function ($tenant) {
-//            if (DB::connection('system')->getSchemaBuilder()->hasTable($tenant->tenancy_db_name)) {
-//                DB::statement("DROP DATABASE ".$tenant->tenancy_db_name);
-//                $tenant->domains()->delete();
-//                $tenant->delete();
-//            }
-//        });
+        Tenant::get()->each(function ($tenant) {
+            $tenant->domains()->delete();
+            $tenant->delete();
+        });
 
 
         foreach (['app', 'demo'] as $domain) {
