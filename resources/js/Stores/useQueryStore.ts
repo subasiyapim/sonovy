27a33b1485f83f869245c5ad5fc8fb8917b28eda
Özkan KÜTÜK <ozkan.kutuk@gmail.com>
2024-10-20 14:32:14 +0300

    fixed

--- resources/js/Stores/useQueryStore.ts
+++ resources/js/Stores/useQueryStore.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import {defineStore} from 'pinia';
+
 export const useQueryStore = defineStore('query', () => {
 
     const search = async (query: any, url: string) => {
@@ -7,41 +8,36 @@ export const useQueryStore = defineStore('query', () => {
             params: {
                 search: query,
             }
-        })
+        });
         return response.data;
     };
 
-    const last = async ( slug: string) => {
-        //params where için kullanılacak
-        const response = await axios.get(slug)
+    const last = async (slug: string) => {
+        // Use params for where condition if needed
+        const response = await axios.get(slug);
         return response.data;
     };
 
-    const find = async (slug: string,payload:object) => {
-        //params where için kullanılacak
-        const response = await axios.post(slug ,{
-            ...payload
-        })
-        return response.data;
-    };
-
-    const post = async(route,payload) => {
-        const response = await axios.post(route ,{
+    const find = async (slug: string, payload: object) => {
+        // Use params for where condition if needed
+        const response = await axios.post(slug, {
             ...payload
         });
         return response.data;
-    }
+    };
 
-
-
-
-
+    // Added explicit types for 'route' and 'payload'
+    const post = async (route: string, payload: object) => {
+        const response = await axios.post(route, {
+            ...payload
+        });
+        return response.data;
+    };
 
     return {
         last,
         search,
         find,
         post
-
     };
 });
