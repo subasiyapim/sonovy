469f9688df941a77cd90180e8aa9dbdfef712e8a
Özkan KÜTÜK <ozkan.kutuk@gmail.com>
2024-10-24 16:50:40 +0300

    CI/CD fixed

--- resources/js/Pages/Front/Index.vue
+++ resources/js/Pages/Front/Index.vue
@@ -1,21 +1,7 @@
-<script setup lang="ts">
+<script setup>
 import {Head, Link, usePage} from '@inertiajs/vue3';
-import DropdownLink from "@/Components/DropdownLink.vue";
 import ApplicationLogo from "@/Components/ApplicationLogo.vue";
 
-defineProps<{
-  canLogin?: boolean;
-  canRegister?: boolean;
-  laravelVersion: string;
-  phpVersion: string;
-}>();
-
-function handleImageError() {
-  document.getElementById('screenshot-container')?.classList.add('!hidden');
-  document.getElementById('docs-card')?.classList.add('!row-span-1');
-  document.getElementById('docs-card-content')?.classList.add('!flex-row');
-  document.getElementById('background')?.classList.add('!hidden');
-}
 </script>
 
 <template>
