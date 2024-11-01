afdc4c07fc7d8021dcce9c567173a874f5db148d
Özkan KÜTÜK <ozkan.kutuk@gmail.com>
2024-11-01 21:04:40 +0300

    fixed

--- app/Http/Controllers/Control/ProductController.php
+++ app/Http/Controllers/Control/ProductController.php
@@ -182,7 +182,7 @@ class ProductController extends Controller
         $languages = getDataFromInputFormat(Country::all(), 'id', 'language', 'emoji');
         $progress = ProductServices::progress($product);
         $platforms = getDataFromInputFormat(Platform::get(), 'id', 'name', 'icon');
-        $product_published_country_types = getDataFromInputFormat(ProductPublishedCountryTypeEnum::getTitles());
+        $product_published_country_types = enumToSelectInputFormat(ProductPublishedCountryTypeEnum::getTitles());
 
 
         $props = [
