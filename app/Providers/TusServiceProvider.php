144aa2a096d105cc3ac720d74c0c754076f2237e
Özkan KÜTÜK <ozkan.kutuk@gmail.com>
2024-12-06 13:53:28 +0300

    improve code

--- app/Providers/TusServiceProvider.php
+++ app/Providers/TusServiceProvider.php
@@ -48,7 +48,7 @@ class TusServiceProvider extends ServiceProvider
         $settings = Cache::remember('settings', 60 * 60 * 24, function () {
             return Setting::whereIn('key',
                 ['allowed_song_formats', 'allowed_ringtone_formats', 'allowed_video_formats'])->get(
-                ['code', 'value']
+                ['key', 'value']
             )->toArray();
         });
 
