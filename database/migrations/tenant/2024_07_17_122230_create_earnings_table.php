0e8681fa3c6988c63377608bafe40df2957dc68e
Özkan KÜTÜK <ozkan.kutuk@gmail.com>
2025-01-21 23:14:56 +0300

    Migration hataları ayıklandı

--- database/migrations/tenant/2024_07_17_122230_create_earnings_table.php
+++ database/migrations/tenant/2024_07_17_122230_create_earnings_table.php
@@ -10,44 +10,45 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        if (!Schema::hasTable('earnings')) {
-            Schema::create('earnings', function (Blueprint $table) {
-                $table->id();
-                $table->unsignedBigInteger('earning_report_id')->nullable();
-                $table->unsignedBigInteger('user_id');
-                $table->string('name')->nullable();
-                $table->date('report_date')->nullable();
-                $table->string('reporting_month', 10)->nullable();
-                $table->date('sales_date')->nullable();
-                $table->string('sales_month', 10)->nullable();
-                $table->string('platform')->nullable();
-                $table->unsignedBigInteger('platform_id')->nullable();
-                $table->string('country')->nullable();
-                $table->string('region', 100)->nullable();
-                $table->unsignedBigInteger('country_id')->nullable();
-                $table->string('label_name')->nullable();
-                $table->unsignedBigInteger('label_id')->nullable();
-                $table->string('artist_name')->nullable();
-                $table->unsignedBigInteger('artist_id')->nullable();
-                $table->string('release_name')->nullable();
-                $table->string('song_name')->nullable();
-                $table->unsignedBigInteger('song_id')->nullable();
-                $table->string('upc_code')->nullable();
-                $table->string('isrc_code')->nullable();
-                $table->string('catalog_number')->nullable();
-                $table->string('release_type')->nullable();
-                $table->string('sales_type')->nullable();
-                $table->integer('quantity');
-                $table->string('currency')->nullable();
-                $table->string('client_payment_currency', 10)->nullable()->default('EUR');
-                $table->decimal('unit_price', 20, 15)->nullable();
-                $table->decimal('mechanical_fee', 20, 12)->nullable();
-                $table->decimal('gross_revenue', 20, 12)->nullable();
-                $table->decimal('client_share_rate', 5, 2)->nullable();
-                $table->decimal('earning', 20, 15);
-                $table->timestamps();
-            });
-        }
+        Schema::create('earnings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('earning_report_id')->nullable();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->date('report_date')->nullable();
+            $table->string('reporting_month', 10)->nullable();
+            $table->date('sales_date')->nullable();
+            $table->string('sales_month', 10)->nullable();
+            $table->unsignedBigInteger('platform_id')->nullable();
+            $table->string('platform')->nullable();
+            $table->string('streaming_type')->nullable();
+            $table->unsignedBigInteger('country_id')->nullable();
+            $table->string('country')->nullable();
+            $table->string('region', 100)->nullable();
+            $table->unsignedBigInteger('label_id')->nullable();
+            $table->string('label_name')->nullable();
+            $table->unsignedBigInteger('artist_id')->nullable();
+            $table->string('artist_name')->nullable();
+            $table->string('release_name')->nullable();
+            $table->unsignedBigInteger('song_id')->nullable();
+            $table->string('song_name')->nullable();
+            $table->string('isrc_code')->nullable();
+            $table->string('upc_code')->nullable();
+            $table->string('catalog_number')->nullable();
+            $table->string('client_payment_currency')->nullable();
+            $table->decimal('quantity', 20, 9)->default(0);
+            $table->decimal('mechanical_fee', 20, 9)->nullable();
+            $table->decimal('gross_revenue', 20, 9)->nullable();
+            $table->decimal('client_share_rate', 5, 2)->nullable();
+            $table->decimal('client_share', 20, 9)->default(0);
+            $table->string('streaming_subscription_type')->nullable();
+            $table->string('release_type')->nullable();
+            $table->string('sales_type')->nullable();
+            $table->string('currency')->nullable();
+            $table->decimal('unit_price', 20, 9)->nullable();
+            $table->decimal('earning', 20, 9)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
     }
 
     /**
