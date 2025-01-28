6577a72bf86b567f05d68ba25e2ade0f7c781617
Özkan KÜTÜK <ozkan.kutuk@gmail.com>
2025-01-28 22:10:30 +0300

    Add indexes to key columns in the earnings table
    This migration improves query performance by adding indexes to frequently queried columns such as platform, country, artist_name, and others in the `earnings` table. The down method ensures proper index removal to maintain rollback capabilities.

---
+++ database/migrations/tenant/2025_01_28_190342_add_index_to_columns_in_table_earnins.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('earnings', function (Blueprint $table) {
+            $table->string('platform', 50)->index()->change();
+            $table->string('country', 50)->index()->change();
+            $table->string('label_name')->index()->change();
+            $table->string('artist_name', 100)->index()->change();
+            $table->string('release_name', 50)->index()->change();
+            $table->string('song_name', 50)->index()->change();
+            $table->string('isrc_code', 15)->index()->change();
+            $table->decimal('earning', 20, 9)->index()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('earnings', function (Blueprint $table) {
+            $table->dropIndex('earnings_platform_index');
+            $table->dropIndex('earnings_country_index');
+            $table->dropIndex('earnings_label_name_index');
+            $table->dropIndex('earnings_artist_name_index');
+            $table->dropIndex('earnings_release_name_index');
+            $table->dropIndex('earnings_song_name_index');
+            $table->dropIndex('earnings_isrc_code_index');
+            $table->dropIndex('earnings_earning_index');
+        });
+    }
+};
