bed41013fe1a6307d83b71ddc401837fe3a35cef
Özkan KÜTÜK <ozkan.kutuk@gmail.com>
2025-02-13 10:58:10 +0300

    sadece test aşamasında çalışacak şekilde güncellendi.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,27 +11,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-            //SYSTEM SEEDERS
+        $seeders = [
+            // SYSTEM SEEDERS
             // \Database\Seeders\System\CountrySeeder::class,
+        ];
 
-            // TENANCY SEEDERS
-            \Database\Seeders\PermissionSeeder::class,
-            \Database\Seeders\RoleSeeder::class,
-            \Database\Seeders\UserSeeder::class,
-            \Database\Seeders\PermissionRoleSeeder::class,
-            \Database\Seeders\RoleUserSeeder::class,
-            \Database\Seeders\PlatformSeeder::class,
-            \Database\Seeders\ContractSeeder::class,
-            \Database\Seeders\FeatureSeeder::class,
-            \Database\Seeders\GenreSeeder::class,
-            \Database\Seeders\IntegrationSeeder::class,
-            \Database\Seeders\LabelSeeder::class,
-            \Database\Seeders\ArtistBranchSeeder::class,
-            \Database\Seeders\ArtistSeeder::class,
-            \Database\Seeders\SettingSeeder::class,
-            \Database\Seeders\ProductSeeder::class,
-            \Database\Seeders\ArtistProductSeeder::class,
-        ]);
+        if (env('APP_URL') !== 'https://sonovy.com') {
+            $seeders = array_merge($seeders, [
+                \Database\Seeders\PermissionSeeder::class,
+                \Database\Seeders\RoleSeeder::class,
+                \Database\Seeders\UserSeeder::class,
+                \Database\Seeders\PermissionRoleSeeder::class,
+                \Database\Seeders\RoleUserSeeder::class,
+                \Database\Seeders\PlatformSeeder::class,
+                \Database\Seeders\ContractSeeder::class,
+                \Database\Seeders\FeatureSeeder::class,
+                \Database\Seeders\GenreSeeder::class,
+                \Database\Seeders\IntegrationSeeder::class,
+                \Database\Seeders\LabelSeeder::class,
+                \Database\Seeders\ArtistBranchSeeder::class,
+                \Database\Seeders\ArtistSeeder::class,
+                \Database\Seeders\SettingSeeder::class,
+                \Database\Seeders\ProductSeeder::class,
+                \Database\Seeders\ArtistProductSeeder::class,
+            ]);
+        }
+
+        $this->call($seeders);
     }
 }
